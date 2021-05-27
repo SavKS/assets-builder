2d64f5d3c2ad6ce5841bcde3e17d893f89611aab
Andrii Savluk <savluk.andrii.v@gmail.com>
2021-05-27 23:51:23 +0300

    Fix style

--- resources/assets/server/routes.js
+++ resources/assets/server/routes.js
@@ -1,16 +1,11 @@
 module.exports = router => {
-    router.get('/cart', async ({ db }) => {
-        const connection = await db();
-
-        // connection.data = {y: 1};
-        //
-        // await connection.write();
-
-        const connection1 = await db('_global', true);
+    router.get('/', async ({ db }) => {
+        const tmpConnection = await db();
+        const layoutsConnection = await db('_global', true);
 
         return [
-            connection.data,
-            connection1.data
+            tmpConnection.data,
+            layoutsConnection.data
         ];
     });
 };
